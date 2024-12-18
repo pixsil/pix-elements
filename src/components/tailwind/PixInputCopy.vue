948fc669cb9a2091e57ee330441ffd03087683de
Luke Braakman <luke@pixsil.nl>
2024-12-18 22:55:17 +0100

    Added input field

---
+++ src/components/tailwind/PixInputCopy.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="relative">
+        <input id="npm-install-copy-button" type="text" v-bind="$attrs" ref="copyInput">
+        <button @click="copyToClipboard" data-copy-to-clipboard-target="npm-install-copy-button" data-tooltip-target="tooltip-copy-npm-install-copy-button" class="absolute end-2 top-1/2 -translate-y-1/2 text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg p-2 inline-flex items-center justify-center">
+            <span v-show="!copied" class="flex items-center">
+                <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                    <path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z"/>
+                </svg>
+            </span>
+            <span v-show="copied" class="flex items-center">
+                <svg class="w-3.5 h-3.5 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5"/>
+                </svg>
+            </span>
+        </button>
+        <div id="tooltip-copy-npm-install-copy-button" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+            <span id="default-tooltip-message">Copy to clipboard</span>
+            <span id="success-tooltip-message" class="hidden">Copied!</span>
+            <div class="tooltip-arrow" data-popper-arrow></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    inheritAttrs: false,
+
+    data() {
+        return {
+            copied: false,
+        };
+    },
+
+    methods: {
+        copyToClipboard() {
+            // const input = this.$refs.copyInput;
+            // input.select();
+            // document.execCommand('copy');
+            this.copied = true;
+
+            setTimeout(() => {
+                this.copied = false;
+            }, 2000);
+        },
+    },
+};
+</script>
+
+<style scoped>
+.tooltip {
+    transition: opacity 0.3s;
+    opacity: 1;
+}
+</style>
--- src/components/tailwind/index.js
+++ src/components/tailwind/index.js
@@ -1,9 +1,11 @@
 // Import Tailwind components
 import PixButton from './PixButton.vue';
 import PixUploadField from './PixUploadField.vue';
+import PixInputCopy from './PixInputCopy.vue';
 
 // Export Tailwind components
 export {
     PixButton,
-    PixUploadField
+    PixUploadField,
+    PixInputCopy,
 };
