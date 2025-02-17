17bbc471b2ba52c27d8fa62579062be74fe416d6
pixsil <57232302+pixsil@users.noreply.github.com>
2025-02-17 17:25:32 +0100

    Update format-helpers.js

--- src/components/helpers/format-helpers.js
+++ src/components/helpers/format-helpers.js
@@ -1,34 +1,15 @@
 
-export function setDecimalComma(object, key, decimals, event) {
-    let value = event.target.value;
-    if (value.match(/^[-]?\d+([.,]\d+)?$/g)) {
-        value = parseFloat(value.replace(',', '.'));
-        value = parseFloat(value.toFixed(decimals));
-    }
-    object[key] = value;
+export function euro(value, decimals = 2) {
+    return '€ ' + Number(value).toFixed(decimals).replace('.', ',');
 }
 
-export function getDecimalComma(object, key) {
-    if (!object[key]) {
-        return;
-    }
-    if (typeof object[key] !== 'number') {
-        return object[key];
-    }
-    return object[key].toFixed(3).replace('.', ',');
-}
-
-export function euro3Decimal(value) {
-    return '€ ' + Number(value).toFixed(3).replace('.', ',');
-}
-
-export function euroThousandPoint(value) {
-    let parts = Number(value).toFixed(2).split('.');
+export function euroThousandPoint(value,  decimals = 2) {
+    let parts = Number(value).toFixed(decimals).split('.');
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, '.');
     return '€ ' + parts.join(',');
 }
 
-export function onlyThousandPoint(value, decimals = 2) {
+export function thousandPoint(value, decimals = 2) {
     let parts = Number(value).toFixed(decimals).split('.');
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, '.');
     return parts.join(',');
